df0b963c66ff282aa0a2fc7b25e53edcb228ac93
Samira <samirasalifoumohamed@gmail.com~>
2026-04-30 20:00:38 +0200

    Corrections pour optimisation SEO

--- script/script.js
+++ script/script.js
@@ -40,8 +40,8 @@ function createSkillsFromJSON() {
                 card.innerHTML = `
                     <div class="card skillsText">
                         <div class="card-body">
-                            <img src="./images/${item.image}" alt="html" width="400" height="400" />
-                            <h4 class="card-title mt-3">${item.title}</h4>
+                            <img src="./images/${item.image}" alt="${item.title}" width="400" height="400" />
+                            <h3 class="card-title mt-3">${item.title}</h4>
                             <p class="card-text mt-3">${item.text}</p>
                         </div>
                     </div>
@@ -75,9 +75,9 @@ function createPortfolioFromJSON() {
                 card.classList.add("col-lg-4", "mt-4");
                 card.innerHTML = `
                     <div class="card portfolioContent">
-                    <img class="card-img-top" src="images/${item.image}" style="width:100%">
+                    <img class="card-img-top" alt="${item.title}" src="images/${item.image}" style="width:100%">
                     <div class="card-body">
-                        <h4 class="card-title">${item.title}</h4>
+                        <h3 class="card-title">${item.title}</h4>
                         <p class="card-text">${item.text}</p>
                         <div class="text-center">
                             <a href="${item.link}" class="btn btn-success">Lien</a>
